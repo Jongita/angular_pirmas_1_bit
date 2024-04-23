1b2f8d032163efb7fa01a158afe8542ab2bb34bb
Jongita <jurgitajonaityte@gmail.com>
2024-04-23 20:00:02 +0300

    Initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SkaiciuokleComponent } from './skaiciuokle/skaiciuokle.component';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SkaiciuokleComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular_pirmas_1_bit';
+  title = 'angular_1_bit';
 }
---
+++ src/app/skaiciuokle/skaiciuokle.component.ts
@@ -0,0 +1,34 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-skaiciuokle',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './skaiciuokle.component.html',
+  styleUrl: './skaiciuokle.component.css'
+})
+export class SkaiciuokleComponent {
+  public x:number|null=null;
+  public y:number|null=null;
+
+  // Kintamasis suma yra arba skaičius arba null reikšmė
+  public suma:number|null=null;
+
+  public skaiciuok(){
+    if (this.x!=null && this.y!=null){
+      this.suma=this.x+this.y;
+      this.x=null;
+      this.y=null;
+    }
+    
+  }
+/*
+  public pridek(){
+    this.x++;
+    console.log(this.x);
+
+  }
+*/
+}
